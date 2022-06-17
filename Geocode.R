0efa6396d0af2822a53e0d2aac3fa248577cc7fe
Amanda Regan <aeregan@clemson.edu>
2022-06-17 15:11:15 -0400

    add unclear data for 81-85

--- Geocode.R
+++ Geocode.R
@@ -66,7 +66,7 @@ for(i in 1:nrow(origAddress)) {
 ##########MANIPULATE UNCLEAR DATA#################
 
 #Make unclear address match the geocoded dataset 
-uncleardata <- read.csv(file = "1-UnclearData/uncleardata-toronto-1965-77.csv", sep = ",", header = TRUE, stringsAsFactors = FALSE)
+uncleardata <- read.csv(file = "1-UnclearData/uncleardata-1981.csv", sep = ",", header = TRUE, stringsAsFactors = FALSE)
 
 #trim white space
 uncleardata <- uncleardata %>%
