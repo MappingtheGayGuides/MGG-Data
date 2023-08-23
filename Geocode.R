ac70a7e0e267d851b40c5e5c4bf2de9ed4178409
Amanda Regan <aeregan@clemson.edu>
2023-08-23 12:56:03 -0400

    Clean up geocode and add notes for Luc

--- Geocode.R
+++ Geocode.R
@@ -24,22 +24,6 @@ origAddress$full.address <- paste(origAddress$streetaddress, ", ", origAddress$c
 # Register the google api code for the georeferencing service.
 register_google(key = Sys.getenv("MGG_GOOGLE_KEY"))
 
-#mgg.geocode <- function(dataset) {
-#  origAddress <- read.csv(paste("2-OrigDataforGeocoding/originaldata-",dataset,".csv", sep=""), header = TRUE)
-#  origAddress <- origAddress %>% mutate_if(is.character, trimws)
-  # paste together the street address, city and state in order to ensure we use full addresses for geocoding. Will minimize mistakes caused by common streetnames. 
-#  origAddress$full.address <- paste(origAddress$streetaddress, ", ", origAddress$city, ", ", origAddress$state, sep="") 
-  # Loop through the addresses to get the latitude and longitude of each address and add it to the origAddress data frame in new columns lat and lon
-#  for(i in 1:10) {
-    # Print("Working...")
-#    result <- tryCatch(geocode(origAddress$full.address[i], output = "latlona", source = "google"), warning = function(w) data.frame(lon = NA, lat = NA, address = NA))
-#    origAddress$lon[i] <- as.numeric(result[1])
-#    origAddress$lat[i] <- as.numeric(result[2])
-#    origAddress$geoAddress[i] <- as.character(result[3])
-#  }
- # fundata <- as.data.frame(origAddress)
-  #return(as.data.frame(fundata))
-#}
 
 # Loop through the addresses to get the latitude and longitude of each address and add it to the origAddress data frame in new columns lat and lon
 for(i in 1:nrow(origAddress)) {
@@ -63,11 +47,6 @@ uncleardata <- uncleardata %>%
 #create full address column
 uncleardata$full.address <- paste(uncleardata$streetaddress, ", ", uncleardata$state, sep="") 
 
-#split Lat/Long out into two columns
-#separate(uncleardata$Lat.Lon, c("lat", "lon"), ",")
-## as of 6-2022 no longer needed?
-#uncleardata <- separate(uncleardata, col = lat.lon, into = c("lat","lon"), sep = ",")
-
 #make sure both dfs have same columns
 origAddress['status'] = 'Geocoded'
 uncleardata['geoAddress'] = 'unclear_coded_by_hand'
@@ -81,18 +60,14 @@ alldata <- rbind(origAddress, uncleardata)
 
 ########MERGE THE TWO DATASETS########
 
-
-#remove empty rows or NAs if there are any
-#origAddress <- origAddress[!apply(is.na(origAddress) | origAddress == "", 1, all),]
-
-#Convert all lon/lat columns to numeric
-#alldata$lat <- as.numeric(alldata$lat)
-#alldata$lon <- as.numeric(alldata$lon)
 # Write a CSV file containing origAddress to the working directory
 write.csv(alldata, "3-GeocodedDatasets/data-1989.csv", row.names=FALSE)
 write.csv(alldata, "4-FullVerifiedDatasets/data-1989.csv", row.names=FALSE)
-#saveRDS(alldata, "DC-Data.rds")
 
+#COMMIT HERE LUC
+
+
+#NO NEED TO CONTINUE, VERIFY IN KEPPLER
 
 maindataset <- read.csv("data.csv", header = TRUE)
 newdata <- read.csv("4-FullVerifiedDatasets/data-1989.csv", header = TRUE)
