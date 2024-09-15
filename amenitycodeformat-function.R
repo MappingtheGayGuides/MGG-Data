e5b21f4540166f42098a71d2e08d87ed2ce8785f
Amanda Regan <aeregan@clemson.edu>
2024-09-15 16:08:18 -0400

    delete extra script

--- amenitycodeformat-function.R
+++ amenitycodeformat-function.R
@@ -1,4 +1,4 @@
-#this function recodes amenity features to include a year range per the data model. For example, the amenity (C) stood for coffee house from 1965-1989. It was absent from the Damron categorizations from 1990-1998. In 1999 it reappeared. But this time it meant Cabaret. This function recodes entries of C to reflect the dates for each category and to distinguish between the two meanings. 
+# this function recodes amenity features to include a year range per the data model. For example, the amenity (C) stood for coffee house from 1965-1989. It was absent from the Damron categorizations from 1990-1998. In 1999 it reappeared. But this time it meant Cabaret. This function recodes entries of C to reflect the dates for each category and to distinguish between the two meanings.
 mggdata <- readRDS("data.rds")
 mgg.amenitycode.format <- function(year1, year2, originalamenity, newamenity) {
   included.data <<- mggdata %>% filter(Year %in% year1:year2)
@@ -10,7 +10,5 @@ mgg.amenitycode.format <- function(year1, year2, originalamenity, newamenity) {
   mggdata <<- rbind(included.data, excluded.data)
   print(paste("data has been tranformed and now has ", length(mggdata$ID), " locations."))
 }
-  mgg.amenitycode.format(1970,1993,"(B)", "(B-1970-1993)")  
-  mgg.amenitycode.format(1965, 1989,"(C)", "(C-1965-1989)")
-
-
+mgg.amenitycode.format(1970, 1993, "(B)", "(B-1970-1993)")
+mgg.amenitycode.format(1965, 1989, "(C)", "(C-1965-1989)")
